626b601dfb1612a23bf58a63be953f624b84c7f5
Mathieu Moalic <matmoa@pm.me>
2025-01-04 18:14:26 +0100

    Set up for release

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.3.0-alpine3.18 as frontend
+FROM node:23.5.0-alpine3.21 as frontend
 WORKDIR /app
 COPY frontend/package.json ./
 COPY frontend/package-lock.json ./
@@ -7,11 +7,10 @@ COPY frontend .
 RUN npm run build
 RUN npm prune --production
 
-FROM python:3.11.3-slim-buster
+FROM python:3.13-slim
 WORKDIR /app
-COPY --from=frontend /app/dist ./static
-COPY backend/requirements.txt ./
+COPY --from=frontend /app/static ./static
+COPY backend/main.py ./
 RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 6001
-COPY backend/main.py ./
 CMD ["uvicorn", "main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "6001"]
--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,6 @@
-# import os
-
 from sqlmodel import Session, SQLModel, create_engine
 
-# db_path = "./test.db"
-# if os.path.exists(db_path):
-#     os.remove(db_path)
-
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./db.sqlite"
 engine = create_engine(DATABASE_URL, echo=False)  # Disable SQL debug logs
 SQLModel.metadata.create_all(engine)
 
@@ -17,26 +11,3 @@ def get_session():
     """
     with Session(engine) as session:
         yield session
-
-
-def init_db_with_data():
-    """
-    Initialize the database with some data.
-    """
-    from backend.models import Category, Item
-
-    with Session(engine) as session:
-        category_1 = Category(name="Category 1")
-        category_2 = Category(name="Category 2")
-        session.add(category_1)
-        session.add(category_2)
-        session.commit()
-
-        item_1 = Item(name="Item 1", category_id=1)
-        item_2 = Item(name="Item 2", category_id=2)
-        session.add(item_1)
-        session.add(item_2)
-        session.commit()
-
-
-# init_db_with_data()
