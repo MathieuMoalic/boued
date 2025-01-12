a0c3d537758927ba7b405c304548046cb02569d4
Mathieu Moalic <matmoa@pm.me>
2025-01-12 13:23:59 +0100

    Fix search bar font color

--- frontend/src/components/Search.svelte
+++ frontend/src/components/Search.svelte
@@ -8,6 +8,6 @@
         placeholder="Search items..."
         bind:value={$searchTerm}
         class="w-full p-2 bg-red-900 border-gray-600 rounded
-               placeholder-gray-400 border-0 focus:outline-none focus:ring-2 focus:ring-red-500"
+               placeholder-gray-400 border-0 focus:outline-none focus:ring-2 focus:ring-red-500 text-primaryText"
     />
 </div>
