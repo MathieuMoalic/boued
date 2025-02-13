ae1769dbfa4c127d05d388626ae962b39fccde83
Mathieu Moalic <matmoa@pm.me>
2025-02-13 13:26:45 +0100

    Hide empty categories

--- frontend/src/components/ActiveItems.svelte
+++ frontend/src/components/ActiveItems.svelte
@@ -16,33 +16,35 @@
 </script>
 
 <main>
-    <List tag="ul" class="text-gray-200 ">
+    <List tag="ul" class="text-gray-200">
         {#each $categories as category}
-            <li class="block">
-                <button
-                    on:click={() => toggleCategory(category.id)}
-                    class="w-full text-left pl-2 flex items-center rounded-md"
-                >
-                    <h5 class="text-lg font-bold text-gray-400">
-                        {category.name}
-                    </h5>
-                    {#if collapsed.includes(category.id)}
-                        <ChevronDownOutline
-                            class="h-6 w-6 transform rotate-180"
-                        />
-                    {:else}
-                        <ChevronDownOutline class="h-6 w-6" />
-                    {/if}
-                </button>
+            {#if $items.some((item) => item.is_active && item.category_id === category.id)}
+                <li class="block">
+                    <button
+                        on:click={() => toggleCategory(category.id)}
+                        class="w-full text-left pl-2 flex items-center rounded-md"
+                    >
+                        <h5 class="text-lg font-bold text-gray-400">
+                            {category.name}
+                        </h5>
+                        {#if collapsed.includes(category.id)}
+                            <ChevronDownOutline
+                                class="h-6 w-6 transform rotate-180"
+                            />
+                        {:else}
+                            <ChevronDownOutline class="h-6 w-6" />
+                        {/if}
+                    </button>
 
-                {#each $items as item}
-                    {#if item.is_active && item.category_id === category.id && !collapsed.includes(category.id)}
-                        <div class="ml-3">
-                            <ItemComp {item} />
-                        </div>
-                    {/if}
-                {/each}
-            </li>
+                    {#each $items as item}
+                        {#if item.is_active && item.category_id === category.id && !collapsed.includes(category.id)}
+                            <div class="ml-3">
+                                <ItemComp {item} />
+                            </div>
+                        {/if}
+                    {/each}
+                </li>
+            {/if}
         {/each}
     </List>
 </main>
